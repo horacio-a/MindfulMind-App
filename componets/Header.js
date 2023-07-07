8dc964592fadcbeef06982f8041e5d6560b4b5b4
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2023-07-07 15:11:42 -0300

    <Feat> [Componets/Header.js]: Back Button Aplic

--- componets/Header.js
+++ componets/Header.js
@@ -1,10 +1,32 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import stylesHeader from '../Styles/HeaderStyle.js'
+import { Icon } from '@rneui/themed';
+import { BackPageState } from '../context/BackPageState';
+import { useContext, useEffect } from 'react';
 
-export default function Header() {
+
+export default function Header({ back, Redirect }) {
+    const { BackPage, setBackPage } = useContext(BackPageState)
+
+
+
+
+
     return (
         <View style={stylesHeader.header}>
-            <View style={stylesHeader.conteinerMenuHeader}></View>
+            {
+                back
+                    ? <TouchableOpacity style={stylesHeader.conteinerMenuHeader} onPress={() => { Redirect(BackPage) }}>
+                        <Icon
+                            name='arrow-back-ios'
+                            color='#fff'
+                            size={18}
+                        />
+                    </TouchableOpacity>
+                    : <View style={stylesHeader.conteinerMenuHeader}>
+                    </View>
+            }
+
             <View style={stylesHeader.conteinerImgHeader}>
                 <Image source={require('../img/xswhite.png')} style={stylesHeader.imgHeader} />
             </View>
