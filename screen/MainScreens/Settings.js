95d56dee32b50b1c51ce2e8ff1a4d84f7ac033eb
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2023-07-31 14:27:53 -0300

    <feat> [Mainscreens/Routine.js]: changesPassword, changeUsername, ChangeProfilePicture

--- screen/MainScreens/Settings.js
+++ screen/MainScreens/Settings.js
@@ -1,24 +1,64 @@
-import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, ImageBackground, Alert } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
-import { useContext, useEffect } from 'react';
-
+import { useContext, useEffect, useState } from 'react';
+import {
+    useFonts,
+    Lato_400Regular,
+    Lato_700Bold,
+} from '@expo-google-fonts/dev';
 import Header from '../../componets/Header';
 import NavBar from '../../componets/navBar';
 import { SesionGlobalState } from '../../context/SesionGlobalState';
 import { Icon } from '@rneui/themed';
 import { BackPageState } from '../../context/BackPageState';
+import ProfileSelect from '../../componets/util/profileSelect';
+import axios from 'axios';
+import ChangePass from '../../componets/util/ChangePass';
+import ChangeUsername from '../../componets/util/ChangeUsername';
+
 
 export default function SettingScreen({ navigation }) {
-    const { session, setsession } = useContext(SesionGlobalState);
-
-
     const { BackPage, setBackPage } = useContext(BackPageState)
+    const { session, setsession } = useContext(SesionGlobalState);
+    const [user, setUser] = useState('')
+    const [profilePicture, setprofilePicture] = useState()
+    const [profilSelectcontent, setprofilSelectcontent] = useState(false)
+    const [ChangePassContent, setChangePassContent] = useState(false)
+    const [ChangeUsernameContent, setChangeUsernameContent] = useState(false)
+
+    useEffect(() => {
+        const funtionUser = async () => {
+            const respose = JSON.parse(await SecureStore.getItemAsync('userToken'))
+            setUser(respose.user)
+
+            switch (respose.profilePicture) {
+                case 'mindfulmindLogo':
+                    setprofilePicture(require('../../assets/profilePictures/s-white.png'))
+                    break;
+                case 'mindfulmindLogoBlack':
+                    setprofilePicture(require('../../assets/profilePictures/s-black.png'))
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        funtionUser()
+    }, [])
+
+
+    let [fontsLoaded] = useFonts({
+        Lato_400Regular,
+        Lato_700Bold
+    });
+
     useEffect(() => {
         setBackPage('SettingScreen')
     }, [])
 
-
-
+    const script = () => {
+        Redirect('Home')
+    }
     const Redirect = (url) => {
         navigation.navigate(url)
     }
@@ -26,32 +66,109 @@ export default function SettingScreen({ navigation }) {
         await SecureStore.deleteItemAsync('userToken')
         setsession(false)
     }
-    return (
-        <>
-            <Header />
 
-            <View style={styles.MainConteiner}>
+    const CuestionExitSession = () => {
+        Alert.alert('Espera', '¿Estas seguro que quieres cerrar sesion?', [
+            { text: 'Si', onPress: () => ExitSession() },
+            {
+                text: 'No',
+                onPress: () => null,
+                style: 'cancel',
+            },
+        ]);
+    }
 
-                <TouchableOpacity onPress={() => { ExitSession() }} style={styles.exitButton}>
-                    <Icon
-                        name={'logout'}
-                        color={'#fff'}
-                        style={{ marginLeft: 25 }} />
-                    <Text style={{ color: '#fff', fontSize: 16, marginLeft: 10 }}>
-                        Cerrar Session
-                    </Text>
-                </TouchableOpacity>
-            </View>
+    const ChangeProfilePicture = async (newImg) => {
+        switch (newImg) {
+            case 'mindfulmindLogo':
+                setprofilePicture(require('../../assets/profilePictures/s-white.png'))
+                break;
+            case 'mindfulmindLogoBlack':
+                setprofilePicture(require('../../assets/profilePictures/s-black.png'))
+                break;
+            default:
+                break;
+        }
+        setprofilSelectcontent(false)
+        const data = JSON.parse(await SecureStore.getItemAsync('userToken'))
+        data.profilePicture = newImg
+        await SecureStore.setItemAsync('userToken', JSON.stringify(data))
 
+        await axios.post('http://31.220.17.121:3500/changeprofilepicture', { data }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+
+
+    if (!fontsLoaded) {
+        return (
+            <></>
+
+        )
+    } else {
+        return (
+            <>
+                <Header back={true} script={script} />
+
+                <View style={styles.MainConteiner}>
+                    <View style={{ width: '85%', height: '90%', display: 'flex', alignItems: 'center' }}>
+
+                        <TouchableOpacity onPress={() => setprofilSelectcontent(true)}>
+                            <Image style={{ width: 200, height: 200, borderRadius: 500 }} source={profilePicture}></Image>
+
+                        </TouchableOpacity>
+
+                        <Text style={{ width: '100%', color: '#1E1E1E', textAlign: 'center', fontSize: 26, fontFamily: 'Lato_700Bold', marginVertical: '5%' }}>
+                            {user}
+                        </Text>
+                        <TouchableOpacity onPress={() => setChangeUsernameContent(true)} style={{ width: '100%', height: 50, backgroundColor: '#1e1e1e', marginTop: 15, borderRadius: 5, display: 'flex', justifyContent: 'center' }}>
+                            <Text style={{ color: '#fff', fontFamily: 'Lato_700Bold', fontSize: 16, marginLeft: 10 }}>Cambiar tu nombre</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => setChangePassContent(true)} style={{ width: '100%', height: 50, backgroundColor: '#1e1e1e', marginTop: 15, borderRadius: 5, display: 'flex', justifyContent: 'center' }}>
+                            <Text style={{ color: '#fff', fontFamily: 'Lato_700Bold', fontSize: 16, marginLeft: 10 }}>Cambiar contraseña</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity onPress={() => { CuestionExitSession() }} style={styles.exitButton}>
+                            <Icon
+                                name={'logout'}
+                                color={'#fff'}
+                                style={{ marginLeft: 25 }} />
+                            <Text style={{ color: '#fff', fontSize: 16, marginLeft: 10 }}>
+                                Cerrar Session
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+
+                </View >
+                {
+                    profilSelectcontent
+                        ? <ProfileSelect setprofilSelectcontent={setprofilSelectcontent} ChangeProfilePicture={ChangeProfilePicture} />
+                        : <></>
+                }
+                {
+                    ChangePassContent
+                        ? <ChangePass setChangePassContent={setChangePassContent} />
+                        : <></>
+                }
+                {
+                    ChangeUsernameContent
+                        ? <ChangeUsername setChangeUsernameContent={setChangeUsernameContent} setUser={setUser} />
+                        : <></>
+                }
+
+                {/* 
             <NavBar
                 Redirect={Redirect}
                 home={false}
                 routine={false}
                 calendar={false}
                 text={false}
-                settings={true} />
-        </>
-    )
+                settings={true} /> */}
+            </>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
@@ -59,7 +176,7 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'flex-end'
+        justifyContent: 'center'
     },
 
     bigblock: {
@@ -72,8 +189,7 @@ const styles = StyleSheet.create({
         width: '100%',
     },
     exitButton: {
-        margin: 3,
-        width: '85%',
+        width: '100%',
         height: 50,
         backgroundColor: '#D01B13',
         color: '#fff',
@@ -81,6 +197,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'flex-start',
         alignItems: 'center',
-        borderRadius: 5
+        borderRadius: 5,
+        marginTop: 150
     }
 });
