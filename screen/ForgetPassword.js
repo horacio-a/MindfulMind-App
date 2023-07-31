dce74aed2150e7952f7e6dd3cec06789f0f0f0f6
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2023-07-31 14:24:45 -0300

    <feat> [Forgetpassword.js]: funcinalidad completa

--- screen/ForgetPassword.js
+++ screen/ForgetPassword.js
@@ -6,8 +6,16 @@ import {
     Lato_400Regular,
     Lato_700Bold,
 } from '@expo-google-fonts/dev';
+import axios from 'axios';
 export default function ForgetPassword({ navigation }) {
     const [Email, setEmail] = useState('')
+    const [authEmail, setauthEmail] = useState(false)
+    const [authCod, setauthCod] = useState(false)
+    const [msgGeneral, setmsgGeneral] = useState(false)
+    const [newPassword, setNewPassword] = useState('')
+    const [ConfirmNewPassword, setConfirmNewPassword] = useState('')
+    const [Cod, setCod] = useState('')
+    const [errorMsg, seterrorMsg] = useState('')
     let [fontsLoaded] = useFonts({
         Lato_400Regular,
         Lato_700Bold
@@ -15,6 +23,87 @@ export default function ForgetPassword({ navigation }) {
     const Redirect = (url) => {
         navigation.navigate(url)
     }
+
+    const sendEmail = async () => {
+
+        if (Email !== '') {
+            const respose = await axios.post('http://31.220.17.121:3500/Authcod/forgetpassword', {
+                "data": {
+                    "user": Email,
+                    "email": Email
+                }
+            }, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            if (respose.data.error) {
+                seterrorMsg(respose.data.error)
+                setTimeout(() => {
+                    seterrorMsg('')
+                }, 3500);
+            } else {
+                setCod(respose.data.code)
+                setauthEmail(true)
+            }
+        } else {
+            seterrorMsg('Complete su email')
+            setTimeout(() => {
+                seterrorMsg('')
+            }, 3500);
+        }
+
+    }
+    const CheckAuthCode = async () => {
+        const respose = await axios.post('http://31.220.17.121:3500/checkAuthcode', {
+            "data": {
+                "token": Cod,
+                "email": Email
+            }
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        if (respose.data.Authcode) {
+            setauthCod(true)
+        } else {
+            seterrorMsg('Codigo incorrecto')
+            setTimeout(() => {
+                seterrorMsg('')
+            }, 3500);
+
+        }
+
+
+    }
+    const changePassword = async () => {
+        if (newPassword === ConfirmNewPassword) {
+            const respose = await axios.post('http://31.220.17.121:3500/ChangePassword', {
+                "data": {
+                    "token": Cod,
+                    "email": Email,
+                    "password": newPassword
+                }
+            }, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            console.log(respose.data)
+            if (respose.data.error) {
+                seterrorMsg(respose.data.msg)
+
+            } else {
+                setmsgGeneral(true)
+            }
+        } else {
+            seterrorMsg('Las contraseñas no coinciden')
+        }
+    }
+
+
     useEffect(() => {
         const backAction = () => {
             Alert.alert('Espera', '¿Estas seguro que quieres salir?', [
@@ -37,28 +126,95 @@ export default function ForgetPassword({ navigation }) {
         return () => backHandler.remove();
     }, []);
 
+
     if (!fontsLoaded) {
         return <></>;
     } else {
         return (
             <>
                 <Header back={true} Redirect={Redirect} />
-                <View style={style.conteinerMain}>
-                    <Text style={style.Title}>Ingrese su Email para recuperar tu cuenta</Text>
-                    <View style={style.ConteinerInput}>
-                        <TextInput style={[style.inputPassword, { fontFamily: 'Lato_400Regular' }]}
-                            onChangeText={setEmail}
-                            placeholder="Email"
-                            keyboardType="default"
-                            placeholderTextColor="rgba(245, 240, 240, 0.75)"
-                        />
-                        <TouchableOpacity onPress={() => { console.log('hola') }} style={style.ButtonSend}>
-                            <Text style={[style.TextButtonSend, { fontFamily: 'Lato_700Bold' }]}>
-                                Enviar
-                            </Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
+                {
+                    authEmail
+                        ? (
+                            authCod
+                                ? (
+                                    msgGeneral
+                                        ?
+                                        <View style={[style.conteinerMain, , { display: 'flex', justifyContent: 'center', alignItems: 'center' }]}>
+                                            <Text style={{ color: '#FFF', fontFamily: 'Lato_700Bold', fontSize: 20 }}>Felicitaciones, tu contraseña fue cambiada</Text>
+                                            <TouchableOpacity onPress={() => { Redirect('SignIn') }} style={[style.ButtonSend, { height: 55, marginBottom: 50 }]}>
+                                                <Text style={[style.TextButtonSend, { fontFamily: 'Lato_700Bold' }]}>
+                                                    Volver al login
+                                                </Text>
+                                            </TouchableOpacity>
+                                        </View>
+
+                                        : <View style={style.conteinerMain}>
+                                            <Text style={style.Title}>Ingrese su nueva contraseña</Text>
+                                            <View style={style.ConteinerInput}>
+                                                <TextInput style={[style.inputPassword,]}
+                                                    onChangeText={setNewPassword}
+                                                    placeholder="Nueva contraseña"
+                                                    keyboardType="default"
+                                                    placeholderTextColor="rgba(245, 240, 240, 0.75)"
+                                                />
+                                                <TextInput style={[style.inputPassword,]}
+                                                    onChangeText={setConfirmNewPassword}
+                                                    placeholder="Confirme su contraseña"
+                                                    keyboardType="default"
+                                                    placeholderTextColor="rgba(245, 240, 240, 0.75)"
+                                                />
+                                                <TouchableOpacity onPress={() => { changePassword() }} style={style.ButtonSend}>
+                                                    <Text style={[style.TextButtonSend, { fontFamily: 'Lato_700Bold' }]}>
+                                                        Enviar
+                                                    </Text>
+                                                </TouchableOpacity>
+                                            </View>
+                                        </View>
+                                )
+                                : <View style={style.conteinerMain}>
+                                    <Text style={style.Title}>Ingrese el codigo que fue enviando a tu Email</Text>
+                                    <View style={style.ConteinerInput}>
+                                        <TextInput style={[style.inputPassword,]}
+                                            onChangeText={setCod}
+                                            placeholder="Codigo de autenticacion"
+                                            keyboardType="default"
+                                            placeholderTextColor="rgba(245, 240, 240, 0.75)"
+                                        />
+                                        <View style={style.conteinerTextError}>
+                                            <Text style={style.TextError}>{errorMsg}</Text>
+                                        </View>
+                                        <TouchableOpacity onPress={() => { CheckAuthCode() }} style={style.ButtonSend}>
+                                            <Text style={[style.TextButtonSend, { fontFamily: 'Lato_700Bold' }]}>
+                                                Enviar
+                                            </Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+                        )
+                        : <View style={style.conteinerMain}>
+                            <Text style={style.Title}>Ingrese su Email para recuperar tu cuenta</Text>
+                            <View style={style.ConteinerInput}>
+                                <TextInput style={[style.inputPassword,]}
+                                    onChangeText={setEmail}
+                                    placeholder="Email"
+                                    keyboardType="default"
+                                    placeholderTextColor="rgba(245, 240, 240, 0.75)"
+                                />
+                                <View style={style.conteinerTextError}>
+                                    <Text style={style.TextError}>{errorMsg}</Text>
+
+                                </View>
+                                <TouchableOpacity onPress={() => { sendEmail() }} style={style.ButtonSend}>
+                                    <Text style={[style.TextButtonSend, { fontFamily: 'Lato_700Bold' }]}>
+                                        Enviar
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                }
+
+
             </>
         )
     }
@@ -66,20 +222,35 @@ export default function ForgetPassword({ navigation }) {
 }
 
 const style = StyleSheet.create({
+    conteinerTextError: {
+        width: '50%',
+        height: 35,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    TextError: {
+        textAlign: 'center',
+        color: '#FFFFFF',
+        fontFamily: 'Lato_700Bold',
+
+    },
     Title: {
         color: '#FFFFFF',
         width: '85%',
         textAlign: 'center',
-        fontSize: 20n
+        fontSize: 18,
+        marginTop: '35%',
+        fontFamily: 'Lato_700Bold'
     },
     conteinerMain: {
         flex: 1,
         backgroundColor: '#1e1e1e',
-
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'flex-start'
     },
     ConteinerInput: {
+        marginTop: '20%',
         width: '85%',
         height: 135,
         display: 'flex',
@@ -94,6 +265,8 @@ const style = StyleSheet.create({
         borderBottomWidth: 3,
         borderBottomColor: '#F5F0F0',
         color: '#F5F0F0',
+        fontFamily: 'Lato_400Regular'
+
     },
     ButtonSend: {
         marginTop: 20,
@@ -110,3 +283,4 @@ const style = StyleSheet.create({
         fontSize: 14,
     },
 })
+
