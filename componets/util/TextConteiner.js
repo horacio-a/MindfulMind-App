dd2e79c387ca6d61c551fbc6a7487798e004e108
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2023-07-31 14:48:42 -0300

    <feat> [componets/util/TextConteiner.js]: text with ...

--- componets/util/TextConteiner.js
+++ componets/util/TextConteiner.js
@@ -5,6 +5,11 @@ export default function TextosConteiners({ data }) {
     return (
         <View style={styleTextComp.ConteinerText}>
             <Text style={styleTextComp.titleText}>{data.title}</Text>
+            <Text style={styleTextComp.Text}>
+                {((data.text).length > 325) ?
+                    (((data.text).substring(0, 325 - 3)) + '...') :
+                    data.text}
+            </Text>
         </View>
     )
 }
