8af0e560f70188d438d7db390f9c129efb876707
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2023-08-25 09:04:42 -0300

    <feat> [screen\Loading.js]: ErrorMsg

--- screen/Loading.js
+++ screen/Loading.js
@@ -1,15 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Button, Text, View, FlatList, ScrollView, SafeAreaView, Image, ActivityIndicator } from 'react-native';
-
+import * as SecureStore from 'expo-secure-store';
 import Constants from 'expo-constants';
+import { useEffect, useState } from 'react';
 
 
 export default function Loading() {
+    const [text, settext] = useState('')
+
+    const deleteuser = async () => {
+        try {
+            const userToken = await SecureStore.getItemAsync('userToken')
+        } catch (error) {
+
+            settext('Error: ' + error)
+            try {
+                SecureStore.deleteItemAsync('userToken')
+            } catch (error) {
+                settext('Error: ' + error)
+            }
+        }
+
+    }
+    useEffect(() => {
+        deleteuser()
+    }, [])
 
     return (
         <SafeAreaView style={styles.container}>
             <Image source={require('../img/mwhite.png')} style={styles.Img} />
             <ActivityIndicator size="large" color={'#fff'} style={styles.Loading} />
+            <Text style={{ color: '#fff' }}>{text}</Text>
         </SafeAreaView>
     );
 }
